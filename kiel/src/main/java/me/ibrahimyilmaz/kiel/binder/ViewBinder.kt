edcc6956f16053bce5fbf2c3abb516b91c7a5815
İbrahim Yilmaz <iylmaz@ymail.com>
2020-07-31 15:01:17 +0200

    in keyword removed and viewBinder getting refactored to be more readable.

--- kiel/src/main/java/me/ibrahimyilmaz/kiel/binder/ViewBinder.kt
+++ kiel/src/main/java/me/ibrahimyilmaz/kiel/binder/ViewBinder.kt
@@ -2,7 +2,7 @@ package me.ibrahimyilmaz.kiel.binder
 
 import android.view.View
 
-abstract class ViewBinder<in T>(private val itemView: View) {
+abstract class ViewBinder<T>(private val itemView: View) {
 
     abstract fun bind(item: T)
     open fun bind(item: T, payload: List<Any>) = Unit
