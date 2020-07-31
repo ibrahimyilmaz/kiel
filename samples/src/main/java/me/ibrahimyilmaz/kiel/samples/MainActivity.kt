29cd414848719871b0f19d20d0d2cadcab8e41af
İbrahim Yilmaz <iylmaz@ymail.com>
2020-07-31 16:49:34 +0200

    Basic RecyclerDataSource example implemented.

---
+++ samples/src/main/java/me/ibrahimyilmaz/kiel/samples/MainActivity.kt
@@ -0,0 +1,43 @@
+package me.ibrahimyilmaz.kiel.samples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.annotation.IdRes
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.navigateUp
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+
+        findViewById<NavigationView>(R.id.nav_view)
+            .setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun findNavController(
+        @Suppress("SameParameterValue") @IdRes id: Int
+    ) = (supportFragmentManager.findFragmentById(id)
+            as NavHostFragment).navController
+
+    override fun onOptionsItemSelected(item: MenuItem) =
+        item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+
+    override fun onSupportNavigateUp() =
+        navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+}
---
+++ samples/src/main/java/me/ibrahimyilmaz/kiel/samples/datasource/model/MessageViewState.kt
@@ -0,0 +1,23 @@
+package me.ibrahimyilmaz.kiel.samples.datasource.model
+
+sealed class MessageViewState {
+
+    data class Poll(
+        val text: String,
+        val options: List<PollOption>,
+        val sentAt: String
+    ) : MessageViewState() {
+        data class PollOption(val text: String)
+    }
+
+    data class Text(
+        val text: String,
+        val sentAt: String
+    ) : MessageViewState()
+
+    data class Image(
+        val text: String,
+        val imageUrl: String,
+        val sentAt: String
+    ) : MessageViewState()
+}
