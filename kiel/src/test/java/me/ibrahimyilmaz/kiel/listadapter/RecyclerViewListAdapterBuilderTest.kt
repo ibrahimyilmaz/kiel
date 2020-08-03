0c44113572983f55665e7d6c201aaa70340ca88c
İbrahim Yilmaz <iylmaz@ymail.com>
2020-08-03 19:08:28 +0200

    unnecessary dependency removed.

--- kiel/src/test/java/me/ibrahimyilmaz/kiel/listadapter/RecyclerViewListAdapterBuilderTest.kt
+++ kiel/src/test/java/me/ibrahimyilmaz/kiel/listadapter/RecyclerViewListAdapterBuilderTest.kt
@@ -1,16 +1,11 @@
 package me.ibrahimyilmaz.kiel.listadapter
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.google.common.truth.Truth.assertThat
 import me.ibrahimyilmaz.kiel.utils.TestRecyclerViewHolder
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 
 class RecyclerViewListAdapterBuilderTest {
-
-    @get:Rule
-    val rule = InstantTaskExecutorRule()
 
     private lateinit var recyclerViewListAdapterBuilder: RecyclerViewListAdapterBuilder<Any>
 
@@ -27,6 +22,7 @@ class RecyclerViewListAdapterBuilderTest {
             viewHolder { ::TestRecyclerViewHolder }
             type { Any::class.java }
         }
+
         // WHEN
         val adapter = recyclerViewListAdapterBuilder.build()
 
